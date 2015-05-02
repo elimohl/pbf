fa9df1e5378075b8609824e18827259124c73c18
elsie <elimohl@gmail.com>
2015-05-03 00:40:00 +0300

    IT WORKS

--- pbf.py
+++ pbf.py
@@ -12,23 +12,66 @@ source_file = open(sys.argv[1], 'r')
 
 
 class Node:
-    def __init__(self, ch):
-        self.ch = ch
+    def __init__(self, name):
+        self.name = name
         self.childrens = []
+
+    def execute(self, cells, tree):
+        if self.name == 'root':
+            for node in self.childrens:
+                node.execute(cells, tree)
+        elif self.name == '>':
+            tree.cell_num += 1
+            if len(cells) < tree.cell_num + 1:  # cell_num >= 0
+                cells.append(0)
+        elif self.name == '<':
+            tree.cell_num -= 1
+            if tree.cell_num < 0:
+                raise Exception('EXECUTION ERROR: Trying to move pointer out of boundary.')
+        elif self.name == '+':
+            cells[tree.cell_num] = (cells[tree.cell_num] + 1) % 256
+        elif self.name == '-':
+            cells[tree.cell_num] = (cells[tree.cell_num] - 1) % 256
+        elif self.name == '.':
+            sys.stdout.write(chr(cells[tree.cell_num]))
+        elif self.name == ',':
+            cells[tree.cell_num] = ord(sys.stdin.read(1))
+        elif self.name == '[]':
+            while cells[tree.cell_num] != 0:
+                for node in self.childrens:
+                    node.execute(cells, tree)
+
+
+class Tree:
+    def __init__(self, root):
+        self.root = root
+        self.cell_num = 0
 
 
 def parse(root):
     while True:
         ch = source_file.read(1)
-        if not ch and root.ch != '':
-            raise Exception('SYNTAX ERROR: Missing "]".')
-        elif ch in ['+', '-', ',', '.', '>', '<', '[', ']']:
+        if not ch:
+            if root.name != 'root':
+                raise Exception('SYNTAX ERROR: Missing "]".')
+            return
+        elif ch in ['>', '<', '+', '-', '.', ',']:
             node = Node(ch)
             root.childrens.append(node)
         if ch == '[':
+            node = Node('[]')
+            root.childrens.append(node)
             parse(node)
         elif ch == ']':
             return
 
-root = Node('')
-parse(root)
+
+try:
+    root = Node('root')
+    tree = Tree(root)
+    parse(root)
+
+    cells = [0]
+    root.execute(cells, tree)
+except Exception as e:
+    print e
